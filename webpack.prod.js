c5ef52a2e2ad22d92963d2f886bfda30b2a6becf
suayipekmekci <suayip.ekmekci@gmail.com>
2018-05-25 12:09:06 +0300

    add splitChunks

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,19 +4,37 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 const CompressionPlugin = require('compression-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: "production",
   devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js',
+  },
+  optimization: {
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    }
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new CleanWebpackPlugin(['dist']),
     new UglifyJSPlugin({
-      cache: false,
-      parallel: false,
-      sourceMap: false,
+      cache: true,
+      parallel: true,
+      sourceMap: true,
       uglifyOptions: {
         warnings: false,
         output: {
@@ -25,6 +43,7 @@ module.exports = merge(common, {
         }
       }
     }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new CompressionPlugin({
         asset: "[path].gz[query]",
         algorithm: "gzip",
